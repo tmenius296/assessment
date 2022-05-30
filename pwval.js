3eac68fefa9b7b57777465d3e7e4a4f2cf8c1edf
Trenton Menius <trenton296@goutlook.com>
2022-05-30 10:31:08 -0400

    Submission Commit

--- pwval.js
+++ pwval.js
@@ -1,10 +1,23 @@
-function CheckPassword(inputtxt) {
-  var passw = /^[1-10]/;
-  if (inputtxt.value.match(passw)) {
-    alert("Correct, try another...");
-    return true;
+/**
+ *1: Into log (prompt user)
+ *2: Wait for user input
+ *3: verify user input
+ *4: On fail, return to step 2
+ *5: On success, success message
+ *6: Exit Program
+ */
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+
+  output: process.stdout,
+});
+
+readline.question("Welcome! Please enter your password\n", function (password) {
+  if (password.length >= 10) {
+    console.log("Grats!");
   } else {
-    alert("Wrong...!");
-    return false;
+    console.log("wrong, loser");
   }
-}
+  readline.close();
+});
